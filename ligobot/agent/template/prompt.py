21f39ce7d2619a01226d8c88f87bc2d73f91b611
Hermes-erm <karan1raider@gmail.com>
2026-08-26 20:29:58 +0530

    prompts

--- ligobot/agent/template/prompt.py
+++ ligobot/agent/template/prompt.py
@@ -1,4 +1,17 @@
 # Make it work first, structure it later
+from pathlib import Path
 
 
-def load_file(file_name: str): ...
+def load_file(file_path: Path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+WORKSPACE_DIR = Path(__file__).resolve().parent / "workspace"
+
+soul = load_file(WORKSPACE_DIR / "SOUL.md")
+agent = load_file(WORKSPACE_DIR / "AGENTS.md")
+
+print(soul)
+print("*" * 70)
+print(agent)
